7cf6beab8b569c6feb22e229ff47e08dc14557cc
Rudra-Prajapati1 <prajapatirudra817@gmail.com>
2025-07-13 10:59:12 +0530

    solved the error

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ import showRouter from "./routes/showRoutes.js";
 import bookingRouter from "./routes/bookingRoutes.js";
 import adminRouter from "./routes/adminRoutes.js";
 import userRouter from "./routes/userRoutes.js";
-import { stripeWebhooks } from "./controllers/StripeWebhooks.js";
+import { stripeWebhooks } from "../controllers/stripeWebhooks.js";
 
 const app = express();
 const PORT = 3000;
